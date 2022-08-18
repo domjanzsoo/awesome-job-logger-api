6f18117a916aaba94e7606db6d5a8cfd1b925052
Zsolt <domjanzsoo@yahoo.com>
2022-08-18 18:52:44 +0100

    migrations

---
+++ database/migrations/2022_08_18_173245_create_jobs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('summary', 150);
+            $table->string('description', 500);
+            $table->enum('status', ['open', 'in progress', 'completed', 'cancelled']);
+            $table->unsignedBigInteger('property_id');
+            $table->timestamps();
+
+            $table->foreign('property_id')->references('id')->on('properties');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
